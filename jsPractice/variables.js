7a57168e5fa990d1f18fe1acd84272a807e261b0
Munib Hassan <munib@practicepanther.com>
2023-10-18 13:37:32 +0500

    Add project files.

---
+++ jsPractice/variables.js
@@ -0,0 +1,63 @@
+var x=10;
+function test(){
+    var y = 20;
+}
+
+console.log(x);
+
+var pop ="hi js";
+function rel(){
+    var top = "hello js";
+    console.log(top);
+}
+rel();
+console.log(pop);
+//console.log(top);
+
+var browser = "chrome";
+var browser = "firefox";
+//browser = "edge"
+console.log(browser);
+
+var g;
+console.log(g);
+g= "hello world"
+console.log(g);
+
+// issue with var:
+var flag = "hye naveen";
+
+var t1 =4;
+if(t1>3){
+    var flag = "hey tom";
+}
+console.log(flag);
+
+//let : 
+//scope: block scoped
+//{}
+let m = "hey Munib";
+let time = 4;
+if(time>3){
+    let mesg = "hey how are you?";
+    console.log(mesg);
+}
+//console.log(mesg);
+console.log(m);
+
+let len =5;
+ len =4;
+
+console.log(len);
+
+//const:
+const mg = "hey naveen"
+//mg = "hey tom"
+
+console.log(mg);
+
+//const must be defined
+const days= 7;
+//days=10;
+console.log(100*7);
+
